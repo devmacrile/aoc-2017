0e3ba580f7645864f9af5ea3b5c64f803d45982d
devmacrile <devin@farmlogs.com>
2017-12-09 18:10:07 -0500

    \w*(?<Day)09

---
+++ solutions/day09/solution.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+
+import os
+import re
+
+
+def get_stream(fname='input.txt'):
+    dir_path = os.path.dirname(os.path.realpath(__file__))
+    fpath = os.path.join(dir_path, fname)
+
+    with open(fpath) as f:
+        stream = f.read().strip()
+
+    return stream
+
+
+def main(stream=get_stream()):
+
+    stream = re.sub('!.{1}', '', stream)
+
+    # remove garbage strings from stream
+    # using '-' as a replacement character
+    in_garbage = False
+    for i, s in enumerate(stream):
+
+        if s == '<' and in_garbage == False:
+            in_garbage = True
+            start = i
+        elif s == '>' and in_garbage == True:
+            in_garbage = False
+            end = i + 1
+            stream = stream[0:start] + '-' * (end - start) + stream[end:]
+
+    stream = stream.replace('-', '')
+
+    # compute the score of the valid groups
+    nest = 1
+    score = 0
+    for i, s in enumerate(stream):
+        if s == '{':
+            nest += 1
+        elif s == '}':
+            nest -= 1
+            score += nest
+
+    return score
+
+
+def test():
+    tests = [("{}", 1),
+             ("{{{}}}", 6), 
+             ("{{},{}}", 5), 
+             ("{{{},{},{{}}}}", 16), 
+             ("{<a>,<a>,<a>,<a>}", 1), 
+             ("{{<ab>},{<ab>},{<ab>},{<ab>}}", 9), 
+             ("{{<!!>},{<!!>},{<!!>},{<!!>}}", 9), 
+             ("{{<a!>},{<a!>},{<a!>},{<ab>}}", 3)]
+    for s, answer in tests:
+        score = main(s)
+        print(s, score, answer)
+        assert score == answer
+        print('\n')   
+
+
+if __name__ == '__main__':
+    #test()
+    print('The total score for all groups is %d!' % main())
