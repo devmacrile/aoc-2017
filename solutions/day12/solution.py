501f9ff47b6eab4e88b3e4910be9704e126871b6
devmacrile <devin@farmlogs.com>
2017-12-12 19:34:20 -0500

    Drummers drumming.

--- solutions/day12/solution.py
+++ solutions/day12/solution.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 def load_adjacency_list(fname='input.txt'):
@@ -17,17 +17,34 @@ def load_adjacency_list(fname='input.txt'):
 
 def main():
     graph = load_adjacency_list()
-    group = set()
+    groups = defaultdict(set)
 
-    def dfs(program):
-        group.add(program)
-        for neighbor in graph[program]:
-            if neighbor not in group:
-                dfs(neighbor)
+    def discovered(vertex):
+        for source, group in groups.iteritems():
+            if vertex in group:
+                return True
+        return False
 
-    dfs('0')
-    return len(group)
+    def bfs(graph, source):
+        q = deque()
+        q.append(source)
+        groups[source].add(source)
+        while len(q) > 0:
+            v = q.popleft()
+            neighbors = graph[v]
+            for w in neighbors:
+                if not discovered(w):
+                    groups[source].add(w)
+                    q.append(w)
+
+    # sorted ensures 0 is first => guaranteed to 
+    # be the leader key for a group
+    for v in sorted(graph.keys()):
+        if not discovered(v):
+            bfs(graph, v)
+            
+    return len(groups['0']), len(groups)
 
 
 if __name__ == '__main__':
-    print('The size of the "0-group" is %d!' % main())
+    print('The size of the "0-group" is %d (%d groups overall)!' % main())
