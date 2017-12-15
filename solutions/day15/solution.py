6ce6141179c8f21ae032112aae8bf34e9f52e3e0
devmacrile <devin@farmlogs.com>
2017-12-15 18:32:59 -0500

    cool that python necessitates no imports

--- solutions/day15/solution.py
+++ solutions/day15/solution.py
@@ -5,19 +5,20 @@ Generator B starts with 629
 """
 
 
-def makegen(start, scale, divisor=2147483647):
+def makegen(start, scale, multiple, divisor=2147483647):
     product = start * scale
     yield product % divisor
     while True:
+        if product % multiple == 0:
+            yield product
         product = (product * scale) % divisor
-        yield product
 
 
-def main(iterations=int(40e6), verbose=True):
+def main(iterations=int(40e6), multiples=(1, 1), verbose=True):
     matches = 0
     divisor = 2 ** 16
-    gena = makegen(289, 16807)
-    genb = makegen(629, 48271)
+    gena = makegen(289, 16807, multiples[0])
+    genb = makegen(629, 48271, multiples[1])
     for i in range(iterations):
         if next(gena) % divisor == next(genb) % divisor:
             matches += 1
@@ -34,4 +35,5 @@ def main(iterations=int(40e6), verbose=True):
 
 
 if __name__ == '__main__':
-    print('The judges final count is %d!' % main())
+    #print('Part1: The judges final count is %d!' % main())
+    print('Part2: The judges final count is %d!' % main(int(5e6), (4, 8)))
