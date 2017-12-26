74e414fba14e1957f7dbcbedb5d7f2a94e575e82
devmacrile <devin@farmlogs.com>
2017-12-26 02:34:17 -0500

    Tricky, tricky - still not sure if...

--- solutions/day18/solution.py
+++ solutions/day18/solution.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
-from collections import defaultdict
+from collections import defaultdict, deque
 
 
 def load_instructions(fname='input.txt'):
@@ -21,6 +21,25 @@ def load_instructions(fname='input.txt'):
     return instructions
 
 
+def evaluate(op, x, val, register, index):
+    if op == 'set':
+        register[x] = val
+    elif op == 'add':
+        register[x] += val
+    elif op == 'mul':
+        register[x] *= val
+    elif op == 'mod':
+        register[x] %= val
+    elif op == 'jgz':
+        if x.isdigit():
+            if int(x) > 0:
+                index += (val - 1)
+        elif register[x] > 0:
+            index += (val - 1)
+
+    return index, register
+
+
 def main():
     instructions = load_instructions()
     register = defaultdict(int)
@@ -38,23 +57,63 @@ def main():
         else:
             op, x, y = instruction
             val = int(y) if y.lstrip('-').isdigit() else register[y]
+            i, register = evaluate(op, x, val, register, i)
 
-            if op == 'set':
-                register[x] = val
-            elif op == 'add':
-                register[x] += val
-            elif op == 'mul':
-                register[x] *= val
-            elif op == 'mod':
-                register[x] %= val
-            else:
-                # jump instruction
-                if register[x] > 0:
-                    i += (val - 1)  # one added by default below
         i += 1
 
     return recovered
 
 
+def messaging():
+
+    def make_register(pid):
+        r = defaultdict(int)
+        r['p'] = pid
+        return r
+
+    def make_generator(register, readq, writeq):
+        i = 0
+        while 0 <= i < len(instructions):
+            if instructions[i][0] in ['snd', 'rcv']:
+                op, x = instructions[i]
+                val = int(x) if x.lstrip('-').isdigit() else register[x]
+            else:
+                op, x, y = instructions[i]
+                val = int(y) if y.lstrip('-').isdigit() else register[y]
+            
+            # dipatch on operation type
+            if op in ['set', 'add', 'mul', 'mod', 'jgz']:
+                i, register = evaluate(op, x, val, register, i)
+            elif op == 'snd':
+                writeq.append(val)
+                yield 's'
+            elif op == 'rcv':
+                if not readq:
+                    yield 'w'
+                else:
+                    register[x] = readq.popleft()
+                    yield 'r'
+
+            i += 1
+
+
+    instructions = load_instructions()
+    q0 , q1= deque(), deque()
+    r0, r1 = make_register(pid=0), make_register(pid=1)
+    p0, p1 = make_generator(r0, q0, q1), make_generator(r1, q1, q0)
+
+    count = 0
+    while True:
+        x, y = next(p0), next(p1)
+        if y == 's':
+            count += 1
+        # check for deadlock
+        if x == 'w' and y == 'w':
+            break
+
+    return count
+
+
 if __name__ == '__main__':
     print('First recovered frequency is %d!' % main())
+    print('Program 1 sends %d times!' % messaging())
